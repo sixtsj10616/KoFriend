60a7817de6c0f45a5b613f8bd9f40b224b41f07b
sixtsj10616 <sixtsj10616@gmail.com>
2024-06-08 14:37:02 +0800

    Initial Commit

---
+++ KKTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KKTest
+//
+//  Created by 劉宥銘 on 2024/6/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
